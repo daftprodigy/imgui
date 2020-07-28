7488ee0c13a92f84e9a58ee2e7f18c2890c4d6c7
daftprodigy <okprodigy@163.com>
2020-07-28 19:15:14 +0800

    change for imgui

--- premake5.lua
+++ premake5.lua
@@ -7,35 +7,21 @@ project "Imgui"
 
 	files
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-		"src/vulkan.c",
-		"src/window.c"
+		"imconfig.h",
+		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h"
 	}
 	filter "system:windows"
 		systemversion "latest"
 		staticruntime "On"
 		cppdialect "c++17" 
-		files
-		{
-			"imconfig.h",
-			"imgui.cpp",
-			"imgui.h",
-			"imgui_demo.cpp",
-			"imgui_draw.cpp",
-			"imgui_internal.h",
-			"imgui_widgets.cpp",
-			"imstb_rectpack.h",
-			"imstb_textedit.h",
-			"imstb_truetype.h"
-		}
-
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
-		buildoptions "/MT"
